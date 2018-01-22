a1e244f83bb36feb7022842a42d96525be4e926a
Maiski88 <yamamoto.mai88@gmail.com>
2018-01-22 07:58:30 -0500

    Generalized merge function. Added rmrare() function

--- initial_process.R
+++ initial_process.R
@@ -36,7 +36,7 @@ cvcalc <- function(input_matrix){
   n = 3
   datalist = list()
   for (i in 3:length(colnames(QCmat))){
-    percv <- round((sd(QCmat[, n])/mean(QCmat[, n]))*100, digits = 2)
+    percv <- round((sd(QCmat[, n], na.rm = TRUE)/mean(QCmat[, n], na.rm = TRUE))*100, digits = 2)
     datalist[[n]] <- percv # add it to your list
     n <- n + 1 
   }
@@ -84,16 +84,13 @@ relativematrix <- function(inputmatrix, outputname) {
 ## END of FUNCTION 
 
 
-# Implementation of the function relativematrix() with your data 
-relativematrix(rawdata, "rawdata")
-
 ###***************** FUNCTION************************
 
 # Function to remove compounds from the relative matrix when %CV of QC exceeds threshhold
 irrep_rm <- function(cvmatrix, CVmax){
   for (compound in colnames(cvmatrix)){
     if (is.na(cvmatrix[2, compound]) || cvmatrix[2, compound] > CVmax) {
-      RPAtotal <- RPAtotal[ , -which(names(RPAtotal) %in% compound)]
+      RPAtotal <<- RPAtotal[ , -which(names(RPAtotal) %in% compound)]
     } 
   }
 }
@@ -103,20 +100,20 @@ irrep_rm <- function(cvmatrix, CVmax){
 
 # Function to combine the replicates and normalize with osmolality 
 ## replicate tolerance applies when there is a large difference between two peaks
-merge_norm <- function(cleanmatrix, osmmatrix, replicate_tolerance, outputname){
+duplmerge <- function(RPAtotal, replicate_tolerance, outputname){
   # Sort the matrix based on "sample" column
-  ordered <- cleanmatrix[with(cleanmatrix, order(sample)), ]
+  ordered <- RPAtotal[with(RPAtotal, order(sample)), ]
   
-  # Remove QC data 
-  samples <- ordered[!grepl("QC", ordered[,3]),]
+  # Remove QC and blank data 
+  samples <- ordered[!grepl("QC|blank|F-Phe|Blank", ordered[, 'sample']),]
   
-  # Take the average of replicates vased on the sample name; 
+  # Take the average of replicates based on the sample name; 
   ## --- take available one if one of the value is NA
-  ## --- take a larger value if %difference from average is > 30%
+  ## --- replace the value with NA if %difference from average is > replicate_tolerance
   
   # subset the matrix
-  RPAlabels <- samples[, 1:3]
-  RPAsamples <- samples[, -c(1:3)]
+  RPAlabels <- samples[, 1:2]
+  RPAsamples <- samples[, -c(1:2)]
   
   # Set up the number of rows and columns for the final matrix
   cmpds <- length(colnames(RPAsamples))
@@ -138,36 +135,45 @@ merge_norm <- function(cleanmatrix, osmmatrix, replicate_tolerance, outputname){
         replmean[z, i] <- "NA" #NA if both replicates are NA
       } else if (is.na(replicate1) || is.na(replicate2)) { #if one of them is integrated, it stands
         replmean[z, i] <- mean(c(replicate1, replicate2), na.rm = TRUE)
-      } else if (abs(((mean(c(replicate1, replicate2)) - replicate1) / mean(c(replicate1, replicate2)))) > replicate_tolerance) {
-        replmean[z, i] <- max(c(replicate1, replicate2)) #if %diff > tolerance (eg. 30%), assign NA
+      } else if (abs(((mean(c(replicate1, replicate2)) - replicate1) / mean(c(replicate1, replicate2)))) > replicate_tolerance*0.01) {
+        replmean[z, i] <- "NA" #if %diff > tolerance (eg. 30%), assign NA
       } else {
         replmean[z, i] <- mean(c(replicate1, replicate2)) #If peaks look pretty much the same, take the mean
       }
-      rownames(replmean)[z] <- as.character(RPAlabels[x, 3])
+      rownames(replmean)[z] <- as.character(RPAlabels[x, 2])
       x = x + 2
       y = y + 2
       z = z + 1
     }
     i = i + 1
   }
-  
-  # Merge osmolality matrix with the matrix created above to ensure the match 
-  merged <- merge(replmean, osmmatrix, by = "row.names")
-  merged[, c(1:2)] <- str_split_fixed(merged$Row.names, "U", 2)
-  colnames(merged)[c(1:2)] <- c("subject", "timepoint")
-  # normalize each value with corresponding osmolality 
-  # normalized <- data.frame(matrix(nrow = samplenum, ncol = cmpds))
-  # colnames(normalized) <- colnames(RPAsamples)
-  # rownames(normalized) <- rownames(replmean)
-  # c = 1
-  # n = 2
-  # for (compd in 1:cmpds) {
-  #   normalized[, c] <- merged[, n] / as.numeric(merged[, 58])
-  #   n = n + 1
-  #   c = c + 1
-  # }
+  meansample <<- replmean
   # ### Final matrix of normalized values for each sample and columns for low volume and dilution observation
-  write.csv(merged, file = outputname)
+  write.csv(replmean, file = paste(outputname,".csv"))
 }
 
 ## END of FUNCTION ####
+
+###***************** FUNCTION************************
+# Function to remove compounds that appears in ~ 75% of samples or less 
+rmrare <- function(inputmatrix, na_perlimit, metabolite_num, outputname){
+  n = 1
+  highna <- NULL
+  naperlist <- NULL
+  rowlength <- length(rownames(inputmatrix))
+  metabmatrix <- inputmatrix[, 2:(metabolite_num+1)]
+  for (i in colnames(metabmatrix)){
+    napercentage <- (sum(is.na(inputmatrix$i))/rowlength)*100
+    if (napercentage > na_perlimit){
+      highna <<- append(highna, i)
+      naperlist <<- append(naperlist, napercentage)
+      n <- n + 1 
+    }
+  }
+  #finalmatrix <<- inputmatrix[, -which(names(inputmatrix) %in% c(highna))]
+  #removed <<- data.frame(inputmatrix[, which(names(inputmatrix) %in% c(highna))], row.names = inputmatrix[, 'sample'])
+  #removedmat <<- rbind(removed, naperlist)
+  #rownames(removedmat)[rowlength+1] <- 'NApercentage'
+  #write.csv(removedmat, file = paste0(outputname, "_highNA.csv"))
+}
+
